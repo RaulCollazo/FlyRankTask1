6060c80d122587aadcadbb1ef52fbcd4a682b5da
RaulCollazo <195011609+RaulCollazo@users.noreply.github.com>
2026-09-14 04:38:14 -0300

    FlyRankTask1

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+@app.get("/health")
+def health():
+    return {"status" : "ok"}
